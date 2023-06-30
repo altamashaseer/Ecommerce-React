d03f3a28bbaebb79d8edc257442c1b7d47ad98f3
Altamash <altamashaseer9@gmail.com>
2023-07-01 01:42:33 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from 'react';
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      Altamash
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
